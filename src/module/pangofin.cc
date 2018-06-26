07e245a6e6ad412e85eb28f001b379558ac8a242
Robert Nsinga <bobuntu@ceable.com>
2018-06-26 17:48:35 +0900

    Like I said, very tricky the first round.

--- src/module/pangofin.cc
+++ src/module/pangofin.cc
@@ -2,14 +2,12 @@
 #include <memory>
 #include <spdlog/spdlog.h>
 
-namespace example
-{
-    int times(int a, int b)
-    {
-        static auto logger = spdlog::stdout_color_mt("times_logger");
-        logger->debug("times called ~ ({}, {})", a, b);
-        int result = a * b;
-        logger->debug("Result: {}", result);
-        return result;
-    }
-}
+namespace Pangofin {
+int times(int a, int b) {
+  static auto logger = spdlog::stdout_color_mt("times_logger");
+  logger->debug("times called ~ ({}, {})", a, b);
+  int result = a * b;
+  logger->debug("Result: {}", result);
+  return result;
+  }
+} // namespace Pangofin
--- test/test_main.cc
+++ test/test_main.cc
@@ -1,2 +1,4 @@
+#ifndef CATCH_CONFIG_MAIN
 #define CATCH_CONFIG_MAIN
 #include <catch.hpp>
+#endif // !CATCH_CONFIG_MAIN
